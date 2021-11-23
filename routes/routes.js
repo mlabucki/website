0ba00daa46f0f32455ea9bb5caaf31a54e4cdf8b
mlabu <labucki.michal@gmail.com>
2021-11-23 15:50:13 +0100

    refacor of routes and users paths

--- routes/routes.js
+++ routes/routes.js
@@ -2,22 +2,20 @@ const express = require('express');
 const router = express.Router();
 const routes = require('../controlles/routes')
 const catchAsync = require('../utils/catchAsync');
-const Route = require('../models/routes');
 const { isLoggedIn, validateRoute, isAuthor } = require('../middleware');
 
-
-router.get('/', catchAsync(routes.index))
+const Route = require('../models/routes');
+router.route('/')
+    .get( catchAsync(routes.index))
+    .post(isLoggedIn, validateRoute, catchAsync(routes.createRoute))
 
 router.get('/new', isLoggedIn, routes.renderNewForm)
 
-router.post('/', isLoggedIn, validateRoute, catchAsync(routes.createRoute))
-
-router.get('/:id', catchAsync(routes.showRoute))
+router.route('/:id')
+    .get(catchAsync(routes.showRoute))
+    .put(isLoggedIn,isAuthor, validateRoute, catchAsync(routes.updateRoute)) 
+    .delete(isLoggedIn,isAuthor, catchAsync(routes.deleteRoute)) 
 
 router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(routes.renderEditForm))
-
-router.put('/:id',isLoggedIn,isAuthor, validateRoute, catchAsync(routes.updateRoute)) 
-
-router.delete('/:id',isLoggedIn,isAuthor, catchAsync(routes.deleteRoute));
 
 module.exports = router;
