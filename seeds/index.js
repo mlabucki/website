0884c3353b3f4a77cb3f6c8592ddaf2319bacaf8
Michal Labucki <labucki.michal@gmail.com>
2021-12-10 19:23:50 +0100

    added sanitize and created extension for joi to block intrusive HTML

--- seeds/index.js
+++ seeds/index.js
@@ -23,7 +23,7 @@ const seedDB = async() => {
        const random10 =  Math.floor(Math.random()*10);
        const distance = Math.floor(Math.random()*20)+ 10;
        const route = new Route({
-           author: '619bb05f60ae8bbf2f1abdbe',
+           author: '61b32023145810f7fdc8f8c2',
            location: `${cities[random10].city}, ${cities[random10].state}`,
            title: `${sample(descriptors)} ${sample(places)}`,
            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
